4126144013851088543e33161c8665a2f6369c67
Manthan29-code <manthanmovaliya9@gmail.com>
2026-03-04 22:55:20 +0530

    add Lifespan event

--- app/main.py
+++ app/main.py
@@ -3,8 +3,14 @@ from app.api.routes import router
 from app.db.session import engine, Base
 from app.models import userModel  ## use app/models/__init__.py in can\se of multiple model import 
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [
     "*"  # Allows all origins
@@ -19,4 +25,7 @@ app.add_middleware(
 )
 
 app.include_router(router)
-Base.metadata.create_all(bind=engine)
+
+# @app.on_event("startup")
+# def startup():
+#     Base.metadata.create_all(bind=engine)
